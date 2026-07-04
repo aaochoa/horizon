29251f0928e5f2277ffce770ac6d863e58978a0a
Anderson Ochoa <anderson@Andersons-MacBook-Pro.local>
2026-07-04 14:56:18 -0500

    Format last updated time to use 12-hour AM/PM format matching Local Time

--- app/javascript/controllers/local_time_controller.js
+++ app/javascript/controllers/local_time_controller.js
@@ -8,10 +8,14 @@ export default class extends Controller {
   connect() {
     const date = new Date(this.utcValue)
     if (!isNaN(date.getTime())) {
-      const hours = String(date.getHours()).padStart(2, '0')
+      let hours = date.getHours()
       const minutes = String(date.getMinutes()).padStart(2, '0')
       const seconds = String(date.getSeconds()).padStart(2, '0')
-      this.element.textContent = `${hours}:${minutes}:${seconds}`
+      const ampm = hours >= 12 ? 'PM' : 'AM'
+      hours = hours % 12
+      hours = hours ? hours : 12 // the hour '0' should be '12'
+      const hoursStr = String(hours).padStart(2, '0')
+      this.element.textContent = `${hoursStr}:${minutes}:${seconds} ${ampm}`
     }
   }
 }
